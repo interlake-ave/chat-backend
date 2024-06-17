42424e24fa18ee9c6e3ab82543f51e77c6739021
Alec Krieger <arkrieg@amazon.com>
2024-06-16 21:02:14 -0700

    Added web socket code for multiple connections and display chat history to new connections

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -3,22 +3,41 @@ package org.example
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import java.util.*
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
-        }
-}
-
+//Code from https://ktor.io/docs/server-websockets.html#handle-multiple-session
 fun main() {
     embeddedServer(Netty, port = 8080) {
+        install(WebSockets)
         routing {
-            get("/") {
-                call.respondText("Hello, world!")
+            val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
+            val chatHistory = Collections.synchronizedList(mutableListOf<String>())
+            webSocket("/chat") {
+                println("Adding user!")
+                val thisConnection = Connection(this)
+                connections += thisConnection
+                try {
+                    send(chatHistory.toString() + "You are connected! There are ${connections.count()} users here.")
+                    for (frame in incoming) {
+                        frame as? Frame.Text ?: continue
+                        val receivedText = frame.readText()
+                        val textWithUsername = "[${thisConnection.name}]: $receivedText"
+                        connections.forEach {
+                            it.session.send(textWithUsername)
+                            chatHistory.add(textWithUsername)
+                        }
+                    }
+                } catch (e: Exception) {
+                    println(e.localizedMessage)
+                } finally {
+                    println("Removing $thisConnection!")
+                    connections -= thisConnection
+                }
             }
         }
     }.start(wait = true)
 }
+
